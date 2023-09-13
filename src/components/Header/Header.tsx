ba3774df21311a7223dc4b9be69759d0a2249464
sgkochar <sgkochar@gmail.com>
2023-09-13 14:15:01 +0300

    Add Header component and Home-main to Home page

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,6 +2,16 @@ import React from 'react'
 import './Header.css'
 
 const Header = () => {
+    const [dropMenu, setDropMenu] = React.useState(false)
+
+    const handleDropMenu = () => {
+        setDropMenu((prevState) => {
+            return (
+                !prevState
+            )
+        })
+    }
+
     return (
         <div className='header'>
             <div className="header-bar d-f jc-sb align-center">
@@ -9,7 +19,9 @@ const Header = () => {
                     <ul className="header-nav-list d-f">
                         <li className="nav-list-item drop-menu-wr d-f">
                             <a href="">Про нас</a>
-                             <button className='drop-menu-btn'><img src="images/arrow-down.png" alt="arrow-down" /></button>
+                            <button className='drop-menu-btn' onClick={handleDropMenu}>
+                                <img src={dropMenu ? "images/arrow-up.png" : "images/arrow-down.png"} alt="arrow-down" />
+                            </button>
                         </li>
                         <li className="nav-list-item"><a href="">Каталог продукції </a></li>
                         <li className="nav-list-item"><a href=""> Оплата і доставка</a></li>
@@ -20,11 +32,16 @@ const Header = () => {
                 </nav>
                 <button className='header-login-btn'>Вхід | Реестрація</button>
             </div>
+            <ul className={dropMenu ?  "drop-menu" : "d-n"} >
+                <li className="drop-menu-item"><a href="">Наша команада</a></li>
+                <li className="drop-menu-item"><a href="">Партнери</a></li>
+                <li className="drop-menu-item"><a href="">Договір</a></li>
+            </ul>
             <div className="header-info d-f jc-sb align-center">
                 <div><img src="images/logo.png" alt="logo" /></div>
                 <form action="" className="header-info-fm">
                     <input type="text" name='searchQuery' placeholder='Пошук' />
-                    <button><img src="images/search.png" alt="search"/></button>
+                    <button><img src="images/search.png" alt="search" /></button>
                 </form>
                 <div className="header-phone-wr d-f">
                     <button><img src="images/phone.png" alt="phone" /></button>
@@ -34,7 +51,7 @@ const Header = () => {
                     </div>
                 </div>
                 <div className='header-info-actions d-f'>
-                    <button className="favourite-btn"><img src="images/heart.png" alt="heart" /></button> 
+                    <button className="favourite-btn"><img src="images/heart.png" alt="heart" /></button>
                     <div className="info-action-wr">
                         <div className="info-action-count">1</div>
                         <div className="info-action-content"><img src="images/comparison.png" alt="comparison" /></div>
@@ -44,7 +61,14 @@ const Header = () => {
                         <div className="info-action-content"><img src="images/cart.png" alt="cart" /></div>
                     </div>
                 </div>
-                
+
+            </div>
+            <div className="header-categories d-f jc-sb">
+                <button className='header-categories-btn'>Насіння</button>
+                <button className='header-categories-btn'>Засоби захисту<br />рослин</button>
+                <button className='header-categories-btn'>Добрива</button>
+                <button className='header-categories-btn'>Кормова група</button>
+                <button className='header-categories-btn'>Агроному<br />в поміч</button>
             </div>
         </div>
     )
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,21 @@
 import React from 'react'
+import './Home.css'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <main>
+      <div className="home-main d-f align-center">
+        <div>
+          <h3 className="home-main-title">Аграрний</h3>
+          <h4 className='home-main-subtitle'>інтернет-магазин</h4>
+          <p className="home-main-desc">
+            Основна сфера діяльності - дистрибуція насіння, засобів захисту рослин, мінеральних макро - та мікродобрив
+          </p>
+          <button className='home-main-btn'>Про компанію</button>
+        </div>
+        <div><img src="images/home-main-img.png" alt="home-main-img" /></div>
+      </div>
+    </main>
   )
 }
 
