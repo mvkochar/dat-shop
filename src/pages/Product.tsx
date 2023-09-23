6b68aa321b3a6d2634cc07a3693f7e9dedcf00b9
sgkochar <sgkochar@gmail.com>
2023-09-23 17:21:37 +0300

    add Product-main block

--- src/pages/Product.tsx
+++ src/pages/Product.tsx
@@ -15,7 +15,7 @@ const Product = () => {
                 <div><img src="images/point.png" alt="point" /></div>
                 <div className="catalog-bredcumb-item bredcumb-item__active">Комманд</div>
             </div>
-            <div className="product-main d-f">
+            <div className="product-main d-f align-center">
                 <div>
                     <div className="product-images-sl d-f align-center">
                         <div><img src="images/arrow-left2.png" alt="arrow-left" /></div>
@@ -32,6 +32,41 @@ const Product = () => {
                     <div className="product-caption d-f align-center">
                         <div><img src="images/home-main-icon.png" alt="home-main-icon" /></div>
                         <h3 className="product-title">Гербіцид Комманд<sup>&#174;</sup>, ФМС УКРАЇНА</h3>
+                    </div>
+                    <div className="d-f jc-sb">
+                        <div className="product-exist">В наявності</div>
+                        <div className="product-price">1 619,03 грн./л</div>
+                    </div>
+                    <div className="product-main-info d-f">
+                        <div className="main-info-item">
+                            <div className="info-item-title">Виробник</div>
+                            <div className="info-item-subtitle info-item-logo">ФМС Украина</div>
+                            <div className="info-item-title" style={{ marginTop: "20px" }}>Оплата</div>
+                            <div className="info-item-payments d-f">
+                                <div><img src="images/payment1.png" alt="payment1" /></div>
+                                <div><img src="images/payment2.png" alt="payment2" /></div>
+                                <div><img src="images/payment3.png" alt="payment3" /></div>
+                            </div>
+                        </div>
+                        <div className="main-info-item">
+                            <div className="info-item-title">Доставка</div>
+                            <div className="info-item-subtitle">Завтра відповідно до тарифів перевізника</div>
+                            <button className='call-btn'>Замовити дзвінок</button>
+                        </div>
+                    </div>
+                    <div className="product-main-actions d-f jc-sb">
+                        <div>
+                            <div className="product-total-price">7814,63 грн</div>
+                            <div className="product-total-desc">Ціна за 5 л</div>
+                        </div>
+                        <div className='d-f align-center' style={{gap: "20px"}}>
+                            <button className='main-actions-bye'>Купити</button>
+                            <button className="favourite-btn"><img src="images/heart1.png" alt="heart1" /></button>
+                            <div className="info-action-wr">
+                                <div className="info-action-count">1</div>
+                                <div className="info-action-content"><img src="images/comparison.png" alt="comparison" /></div>
+                            </div>
+                        </div>
                     </div>
                 </div>
             </div>
